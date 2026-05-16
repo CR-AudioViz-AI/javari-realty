a614f817b13001555b843ded035ec7844d812956
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-16 17:01:09 -0400

    fix: supabase SSG + serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-/** @type {import("next").NextConfig} */
-const nextConfig={typescript:{ignoreBuildErrors:true},eslint:{ignoreDuringBuilds:true},reactStrictMode:false}
-module.exports=nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: false,
+  // Prevent static generation of API routes
+  output: undefined,
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/supabase-js'],
+  },
+}
+module.exports = nextConfig
